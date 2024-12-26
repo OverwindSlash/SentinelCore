a516683cdb7ad3833a65f0e133855017dbd2acf3
Overwind Slash <overwind@outlook.com>
2024-12-26 15:36:11 +0800

    TODO: implement ocr handler

---
+++ src/handler/Handler.Ocr/Algorithms/PaddleSharpOcrAlg.cs
@@ -0,0 +1,27 @@
+﻿using SentinelCore.Domain.Abstractions.AnalysisHandler;
+using SentinelCore.Domain.Entities.AnalysisEngine;
+using SentinelCore.Domain.Entities.VideoStream;
+
+namespace Handler.Ocr.Algorithms
+{
+    public class PaddleSharpOcrAlg : IAnalysisHandler
+    {
+        public string HandlerName { get; }
+
+        public void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnalysisResult Analyze(Frame frame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
